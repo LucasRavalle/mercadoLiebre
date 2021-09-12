779bea1f7fd2d01d02177faa631bcd1b0cf639db
LucasRavalle <ravallelucas@gmail.com>
2021-09-12 16:24:29 -0300

    subo trabajo formularios

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express= require ('express');
 const app= express();
 const path= require ('path');
-const port= 3000;
+const port= 3030;
 
 app.use(express.static('public'))
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
 
 app.listen(port, () => console.log('Servidor funcionando en el puerto http://localhost:' + port))
